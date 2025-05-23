5c64e06ec27d51aa8daac708bdc815b611aee820
lochhh <changhuan.lo@ucl.ac.uk>
2025-05-23 16:14:25 +0100

    Create pos and conf arrays

--- aeon_to_movement.py
+++ aeon_to_movement.py
@@ -17,30 +17,20 @@ pose = io_api.load(
 )
 pose = pose[pose.identity.isin(["BAA-1104569", "BAA-1104568"])]
 pose = pose[~pose["part"].str.startswith("anchor_")]
+# Out-of-order timestamps so full chunk returned
 pose.sort_index(inplace=True)
-
+pose = pose.loc["2024-07-02 14:00:00":"2024-07-02 14:10:00"]
+# Convert index to seconds
+pose.index = (pose.index - pose.index[0]).total_seconds()
 # Infer FPS
-frames = pose.index.drop_duplicates()
-time_diff = frames.to_series().diff().dt.total_seconds()
-mean_time_diff = time_diff[1:].mean()
+frames = pose.index.drop_duplicates().to_numpy()
+mean_time_diff = np.diff(frames).mean()
 fps = float(np.ceil(1 / mean_time_diff)) if mean_time_diff > 0 else None
 
 keypoints = pose["part"].unique()
 individuals = pose["identity"].unique()
-# Create position array
-# Slow...
-frame_idx = {v: i for i, v in enumerate(frames)}
-kp_idx = {v: i for i, v in enumerate(keypoints)}
-ind_idx = {v: i for i, v in enumerate(individuals)}
-pos = np.full((len(frames), 2, len(keypoints), len(individuals)), np.nan, dtype=float)
-for idx, row in pose.iterrows():
-    f = frame_idx[idx]
-    kp = kp_idx[row["part"]]
-    ind = ind_idx[row["identity"]]
-    pos[f, 0, kp, ind] = row["x"]
-    pos[f, 1, kp, ind] = row["x"]
 
-# Faster?
+# Create position array
 multi_idx = pd.MultiIndex.from_product(
     [frames, keypoints, individuals], names=["frame", "part", "identity"]
 )
@@ -49,17 +39,7 @@ pose_full = pose_reset.reindex(multi_idx)
 x_arr = pose_full["x"].values.reshape(len(frames), len(keypoints), len(individuals))
 y_arr = pose_full["y"].values.reshape(len(frames), len(keypoints), len(individuals))
 pose_arr = np.stack([x_arr, y_arr], axis=1).astype(float)
-np.allclose(pose_arr, pos, equal_nan=True)
-
-# from_numpy args
-# position_array (np.ndarray) – Array of shape (n_frames, n_space, n_keypoints, n_individuals) containing the poses. It will be converted to a xarray.DataArray object named “position”.
-
-# confidence_array (np.ndarray, optional) – Array of shape (n_frames, n_keypoints, n_individuals) containing the point-wise confidence scores. It will be converted to a xarray.DataArray object named “confidence”. If None (default), the scores will be set to an array of NaNs.
-
-# individual_names (list of str, optional) – List of unique names for the individuals in the video. If None (default), the individuals will be named “individual_0”, “individual_1”, etc.
-
-# keypoint_names (list of str, optional) – List of unique names for the keypoints in the skeleton. If None (default), the keypoints will be named “keypoint_0”, “keypoint_1”, etc.
-
-# fps (float, optional) – Frames per second of the video. Defaults to None, in which case the time coordinates will be in frame numbers.
-
-# source_software (str, optional) – Name of the pose estimation software from which the data originate. Defaults to None.
+# Create confidence array
+conf_arr = pose_full["part_likelihood"].values.reshape(
+    len(frames), len(keypoints), len(individuals)
+)
