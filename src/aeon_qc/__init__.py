ca86b5ddbe19cfb204dee94cdc6c133eeed63f37
NeuroThom <t.ryan@neurogears.org>
2026-04-08 17:28:00 +0100

    Export run_qc and generate_report

--- src/aeon_qc/__init__.py
+++ src/aeon_qc/__init__.py
@@ -5,7 +5,7 @@ from aeon_qc.environment import environment_state_durations, message_log_errors
 from aeon_qc.epochs import epoch_gaps
 from aeon_qc.heartbeat import heartbeat_gaps
 from aeon_qc.pellet import pellet_failures
-from aeon_qc.schemas import schema_from_metadata, schema_from_root
+from aeon_qc.report import generate_report, run_qc
 from aeon_qc.sync import sync_delta
 from aeon_qc.video import dropped_frames
 
@@ -18,6 +18,6 @@ __all__ = [
     "pellet_failures",
     "message_log_errors",
     "environment_state_durations",
-    "schema_from_metadata",
-    "schema_from_root",
+    "run_qc",
+    "generate_report",
 ]
